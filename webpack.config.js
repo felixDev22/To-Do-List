5e5b4e3fe4a71e837b230e642be37649ec573ca3
felixDev22 <felixnzomo@gmail.com>
2022-12-08 14:39:09 +0300

    Set index.html using HTML webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -6,5 +7,12 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'To-do-list',
+      filename: 'index.html',
+    }),
+  ],
 };
