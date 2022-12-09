05f79326152c1f2ca15c74796106cf7d18424637
felixDev22 <felixnzomo@gmail.com>
2022-12-09 20:10:25 +0300

    Add detele variable

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './style.css';
+
+const listItems = document.querySelector('.task-list');
+const addTask = document.querySelector('#new-task');
+const addBtn = document.getElementById('plus');
+const deleteTask = document.querySelector('.trash');
