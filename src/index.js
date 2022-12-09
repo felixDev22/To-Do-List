9c836a83b01ebc2889abea35c7947fe29b51a05c
felixDev22 <felixnzomo@gmail.com>
2022-12-09 20:22:45 +0300

    Add empty array, Add a function to push to LocalStorage

--- src/index.js
+++ src/index.js
@@ -4,3 +4,16 @@ const listItems = document.querySelector('.task-list');
 const addTask = document.querySelector('#new-task');
 const addBtn = document.getElementById('plus');
 const deleteTask = document.querySelector('.trash');
+
+let taskArr = [];
+
+const addNewList = () => {
+  const task = {};
+  task.index = taskArr.length + 1;
+  task.description = addTask.value;
+  task.completed = false;
+  taskArr.push(task);
+};
+const pushToLocal = () => {
+  localStorage.setItem('taskArr', JSON.stringify(taskArr));
+};
