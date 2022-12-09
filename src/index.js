a4778e89ad19d491c982ff0a5d03237dead4a9ee
felixDev22 <felixnzomo@gmail.com>
2022-12-09 14:37:29 +0300

    Add index.js functions, Update style.css & index.html file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,52 @@
-import './index.css';
+import './style.css';
+
+const listItems = document.querySelector('.task-list');
+const addTask = document.querySelector('#new-task');
+const addBtn = document.getElementById('plus');
+
+let taskArr = [];
+
+const addNewList = () => {
+  const task = {};
+  task.index = taskArr.length + 1;
+  task.description = addTask.value;
+  task.completed = false;
+  taskArr.push(task);
+};
+const pushToLocal = () => {
+  localStorage.setItem('taskArr', JSON.stringify(taskArr));
+};
+
+const generateList = () => {
+  listItems.innerHTML = '';
+  taskArr.forEach((task) => {
+    const toDo = `<li class="task" >
+          <div class='item' >
+          <input type="Checkbox" id="1" />
+          <p>${task.description}</p>
+          </div>
+          <div class='trash' ${task.index}>
+            <i class="fa-regular fa-trash-can"></i>
+          </div>
+        </li>`;
+    listItems.innerHTML += toDo;
+    addTask.value = '';
+  });
+};
+
+const showList = () => {
+  if (localStorage.getItem('taskArr')) {
+    taskArr = JSON.parse(localStorage.getItem('taskArr'));
+  }
+  generateList();
+};
+
+window.addEventListener('load', () => {
+  showList();
+});
+
+addBtn.addEventListener('click', () => {
+  addNewList();
+  generateList();
+  pushToLocal();
+});
